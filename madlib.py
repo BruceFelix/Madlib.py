ff527fc291aa1aebe8ee297d2645b13b294fca4b
Bruce Felix Macharia <44478872+BruceFelix@users.noreply.github.com>
2020-10-20 18:50:15 +0300

    Add files via upload

--- madlib.py
+++ madlib.py
@@ -14,35 +14,54 @@ Love,
 
 NAME
 """
-newProseString = proseString # assigns the prose string to a new variable
+# newProseString = proseString # assigns the prose string to a new variable
 
-userInput = input("Please provide an example of an occupation.")
-newProseString = newProseString.replace("OCCUPATION", userInput)# prompts the user to input their occupation
-#  the rest are quite straight forward but incase you want some elaboration hit my github page I will get back to you
-userInput = input("Please provide a country.")
-newProseString = newProseString.replace("COUNTRY", userInput)
+# userInput = input("Please provide an example of an occupation.")
+# newProseString = newProseString.replace("OCCUPATION", userInput)# prompts the user to input their occupation
+# #  the rest are quite straight forward but incase you want some elaboration hit my github page I will get back to you
+# userInput = input("Please provide a country.")
+# newProseString = newProseString.replace("COUNTRY", userInput)
 
-userInput = input("Please provide a plural noun.")
-newProseString = newProseString.replace("PLURAL_NOUN", userInput)
+# userInput = input("Please provide a plural noun.")
+# newProseString = newProseString.replace("PLURAL_NOUN", userInput)
 
-userInput = input("Please provide  a verb.")
-newProseString = newProseString.replace("VERB", userInput)
+# userInput = input("Please provide  a verb.")
+# newProseString = newProseString.replace("VERB", userInput)
 
-userInput = input("Please provide an objective.")
-newProseString = newProseString.replace("OBJECTIVE", userInput)
+# userInput = input("Please provide an objective.")
+# newProseString = newProseString.replace("OBJECTIVE", userInput)
 
-userInput = input("Please provide an example of a personal item.")
-newProseString = newProseString.replace("PERSONAL_ITEM", userInput)
+# userInput = input("Please provide an example of a personal item.")
+# newProseString = newProseString.replace("PERSONAL_ITEM", userInput)
 
-userInput = input("Please provide a holiday.")
-newProseString = newProseString.replace("HOLIDAY", userInput)
+# userInput = input("Please provide a holiday.")
+# newProseString = newProseString.replace("HOLIDAY", userInput)
 
-userInput = input("What's is your name?.")
-newProseString = newProseString.replace("NAME", userInput)
+# userInput = input("What's is your name?.")
+# newProseString = newProseString.replace("NAME", userInput)
 
-print(newProseString)
+# print(newProseString)
 
 #This program was ment to teach me about iteration. 
 #It might not be the best way to go around this problem but I chose to use it to learn.
 # As iteration is about refinement I am not stopping at this for I need to find a solution
 # to work with larger pieces of data and more iterations but this is the end of this scope for now.
+
+replacements = [
+    ["An occupation", "OCCUPATION"],
+    ["A country", "COUNTRY"],
+    ["A plural noun", "PLURAL_NOUN"],
+    ["A verb, like 'run,' 'eat' or 'think'", "VERB"],
+    ["An adjectiv, like 'friendly,' 'long,' or 'warm'", "ADJECTIVE"],
+    ["A title that someone might have in an organization, like 'manager,' 'captain,' or 'trainer'", 'TITLE'],
+    ["A personal item"," PERSONAL_ITEM"],
+    ["A holiday, like Christmas or Labor Day", "HOLIDAY"],
+    ["Your name", "NAME"]
+]
+
+for prompt,placeHolder in replacements:
+    userInput = input(prompt)
+
+    proseString = proseString.replace(placeHolder,userInput)
+    
+print(proseString)
